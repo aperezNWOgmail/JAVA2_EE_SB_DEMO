1efe7780dd8129ead01f423d61f506256b1a036d
apereznwo <57961103+aperezNWO@users.noreply.github.com>
2024-11-04 08:53:10 +0000

    encoding fix

--- src/main/java/com/example/demo/DemoEndPoint.java
+++ src/main/java/com/example/demo/DemoEndPoint.java
@@ -35,8 +35,8 @@ public class DemoEndPoint {
         Charset utf8Charset = StandardCharsets.UTF_8;
 
         // Encode a string into a byte array
-        //String text = "Hello, World! ■";
-        String text = "Hello, World! &#9679;";
+        String text = "Hello, World! &#x25A0; &#x2261;";
+
         byte[] encodedBytes = text.getBytes(utf8Charset);
 
         // Decode a byte array into a string
@@ -73,6 +73,21 @@ public class DemoEndPoint {
     //
     @GetMapping("/GenerateRandomVertex_SpringBoot")
     public String GenerateRandomVertex_SpringBoot() {
-        return "[2,20]|[15,22]|[1,17]|[8,19]|[14,6]|[13,8]|[5,12]|[4,14]|[22,5]■{0,0,0,6,18,16,0,0,0}|{0,0,14,0,0,0,0,0,18}|{0,14,0,0,0,0,6,0,24}|{6,0,0,0,0,12,0,6,0}|{18,0,0,0,0,0,0,0,0}|{16,0,0,12,0,0,8,0,0}|{0,0,6,0,0,8,0,2,18}|{0,0,0,6,0,0,2,0,0}|{0,18,24,0,0,0,18,0,0}■00&lt;[2;20]&gt;-00-<br/>01&lt;[15;22]&gt;-34-[0;3]≡[3;7]≡[7;6]≡[6;2]≡[2;1]≡<br/>02&lt;[1;17]&gt;-20-[0;3]≡[3;7]≡[7;6]≡[6;2]≡<br/>03&lt;[8;19]&gt;-06-[0;3]≡<br/>04&lt;[14;6]&gt;-18-[0;4]≡<br/>05&lt;[13;8]&gt;-16-[0;5]≡<br/>06&lt;[5;12]&gt;-14-[0;3]≡[3;7]≡[7;6]≡<br/>07&lt;[4;14]&gt;-12-[0;3]≡[3;7]≡<br/>08&lt;[22;5]&gt;-32-[0;3]≡[3;7]≡[7;6]≡[6;8]≡";
+
+        // Get a Charset object for UTF-8 encoding
+        Charset utf8Charset = StandardCharsets.US_ASCII;
+
+        // Encode a string into a byte array
+        // String text = "Hello, World! &#x25A0; &#x2261;";
+
+        String text = "[2,20]|[15,22]|[1,17]|[8,19]|[14,6]|[13,8]|[5,12]|[4,14]|[22,5]&#x25A0;{0,0,0,6,18,16,0,0,0}|{0,0,14,0,0,0,0,0,18}|{0,14,0,0,0,0,6,0,24}|{6,0,0,0,0,12,0,6,0}|{18,0,0,0,0,0,0,0,0}|{16,0,0,12,0,0,8,0,0}|{0,0,6,0,0,8,0,2,18}|{0,0,0,6,0,0,2,0,0}|{0,18,24,0,0,0,18,0,0}&#x25A0;00&lt;[2;20]&gt;-00-<br/>01&lt;[15;22]&gt;-34-[0;3]&#x2261;[3;7]&#x2261;[7;6]&#x2261;[6;2]&#x2261;[2;1]&#x2261;<br/>02&lt;[1;17]&gt;-20-[0;3]&#x2261;[3;7]&#x2261;[7;6]&#x2261;[6;2]&#x2261;<br/>03&lt;[8;19]&gt;-06-[0;3]&#x2261;<br/>04&lt;[14;6]&gt;-18-[0;4]&#x2261;<br/>05&lt;[13;8]&gt;-16-[0;5]&#x2261;<br/>06&lt;[5;12]&gt;-14-[0;3]&#x2261;[3;7]&#x2261;[7;6]&#x2261;<br/>07&lt;[4;14]&gt;-12-[0;3]&#x2261;[3;7]&#x2261;<br/>08&lt;[22;5]&gt;-32-[0;3]&#x2261;[3;7]&#x2261;[7;6]&#x2261;[6;8]&#x2261;";
+
+        byte[] encodedBytes = text.getBytes(utf8Charset);
+
+        // Decode a byte array into a string
+        String decodedText = new String(encodedBytes, utf8Charset);
+
+        return decodedText;
+
     }
 }
